72eeb510d8fa607016c824d02623d3bcf6137b47
Alexander A. Klimov <alexander.klimov@icinga.com>
2024-02-20 12:41:41 +0100

    [Refactor] CpuBoundWork#CpuBoundWork(): require an io_context::strand

--- lib/remote/httpserverconnection.cpp
+++ lib/remote/httpserverconnection.cpp
@@ -331,7 +331,8 @@ bool EnsureValidBody(
 	ApiUser::Ptr& authenticatedUser,
 	boost::beast::http::response<boost::beast::http::string_body>& response,
 	bool& shuttingDown,
-	boost::asio::yield_context& yc
+	boost::asio::yield_context& yc,
+	boost::asio::io_context::strand& strand
 )
 {
 	namespace http = boost::beast::http;
@@ -423,7 +424,8 @@ bool ProcessRequest(
 	CpuBoundWork*& m_HandlingRequest,
 	bool& hasStartedStreaming,
 	std::chrono::steady_clock::duration& cpuBoundWorkTime,
-	boost::asio::yield_context& yc
+	boost::asio::yield_context& yc,
+	boost::asio::io_context::strand& strand
 )
 {
 	namespace http = boost::beast::http;
@@ -431,7 +433,7 @@ bool ProcessRequest(
 	try {
 		// Cache the elapsed time to acquire a CPU semaphore used to detect extremely heavy workloads.
 		auto start (std::chrono::steady_clock::now());
-		CpuBoundWork handlingRequest (yc);
+		CpuBoundWork handlingRequest (yc, strand);
 		cpuBoundWorkTime = std::chrono::steady_clock::now() - start;
 
 		Defer resetHandlingRequest ([&m_HandlingRequest] { m_HandlingRequest = nullptr; });
@@ -548,13 +550,13 @@ void HttpServerConnection::ProcessMessages(boost::asio::yield_context yc)
 				break;
 			}
 
-			if (!EnsureValidBody(*m_Stream, buf, parser, authenticatedUser, response, m_ShuttingDown, yc)) {
+			if (!EnsureValidBody(*m_Stream, buf, parser, authenticatedUser, response, m_ShuttingDown, yc, m_IoStrand)) {
 				break;
 			}
 
 			m_Seen = std::numeric_limits<decltype(m_Seen)>::max();
 
-			if (!ProcessRequest(*m_Stream, request, authenticatedUser, response, *this, m_HandlingRequest, m_HasStartedStreaming, cpuBoundWorkTime, yc)) {
+			if (!ProcessRequest(*m_Stream, request, authenticatedUser, response, *this, m_HandlingRequest, m_HasStartedStreaming, cpuBoundWorkTime, yc, m_IoStrand)) {
 				break;
 			}
 
